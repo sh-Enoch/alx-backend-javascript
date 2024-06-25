e987616d2d80d321379314506a57080eb0017d2f
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-06-25 02:03:45 +0000

    all

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
-  var task;
-  var task2;
+  const task = false;
+  const task2 = true;
 
-  return [task, task2];
+  if (trueOrFalse) {
+    /*eslint-disable */
+    const task = true;
+    const task2 = false;
+    }
+
+    return [task, task2];
 }
