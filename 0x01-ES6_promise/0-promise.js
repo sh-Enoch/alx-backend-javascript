215fae536904b55e0e24e5f427452ea2ed98aae2
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-06-26 22:10:19 +0000

    <created 1 promise>

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,13 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        /* eslint-disable */
-        if (true) {
-            resolve();
-        } else {
-            reject();
-        }
-        /* eslint-enable */
-    });
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+	reject();
+      }
+      /* eslint-enable */
+  });
 }
 
 export default getResponseFromAPI;
