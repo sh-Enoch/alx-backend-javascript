f2669f58cf51d1e84376f8a09c1d2350f27e82de
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-06-26 21:47:54 +0000

    <modified> promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,9 @@
-export default function getResponseFromAPI() {
+function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
          resolve('https://google.com');
          }, 200)
     });
 }
+
+export default getResponseFromAPI;
