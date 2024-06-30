147ccce5f184fa38c32ef4d78871019e069df4d5
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-06-30 19:30:12 +0000

    <create> class: ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+    _maxStudentSize;
+    constructor (maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,2 @@
+ES6 classes.
+all about classes.
