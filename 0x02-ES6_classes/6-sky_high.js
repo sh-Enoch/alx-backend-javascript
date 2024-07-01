16afc5ede6f40ed8878d62dcab55e99b9c928510
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-01 20:58:30 +0000

    <create> class: SkyHighBuilding

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    } else {
+      this._floors = value;
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
