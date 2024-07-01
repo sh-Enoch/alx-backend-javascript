d74c2c903f1dbc1a0fede9a51811ecca69c8e35e
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-01 11:14:20 +0000

    <create> class: Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = value;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+}
