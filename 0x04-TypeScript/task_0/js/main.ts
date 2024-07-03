1a590a38719aad1d124f8465f112a0e49151d9b1
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-07-03 21:54:45 +0000

    <created> file:main.ts

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1 : Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    location: "Lagos"
+}
+
+const student2 : Student = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 22,
+    location: "Lagos"
+}
+
+const studentList = [student1, student2];
+
+const table = document.createElement('table');
+
+const headerRow = table.insertRow();
+const nameHeader = headerRow.insertCell();
+const locationHeader = headerRow.insertCell();
+nameHeader.textContent = "First Name";
+locationHeader.textContent = "Location";
+
+studentList.forEach(student => {
+    const row = table.insertRow();
+    const nameCell = row.insertCell();
+    const locationCell = row.insertCell();
+
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+});
+document.body.appendChild(table);
