e328c28a91dd64cf407962fa48f88ff5611a19f6
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-09-18 07:31:05 +0000

    create server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,8 +3,10 @@ const { createServer } = require('node:http');
 const port = 1245;
 const hostname = '127.0.0.1';
 
-const server = createServer((req, res) => {
+const app = createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname);
+app.listen(port, hostname);
+
+module.exports = app;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,7 @@
+const createServer = require(node:http);
+
+const port = 1245;
+const hostname = '127.0.0.1';
+
+const server = createServer((req, res) => {
+    res.})
