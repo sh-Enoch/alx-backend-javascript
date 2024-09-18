d1544ffddf4a087a5fb9fcee71f65f8f3d2d1e7c
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-09-18 12:34:26 +0000

    http

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,10 @@
-const process = require('')
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Welcome to Holberton School, what is your name?\n', name => {
+    console.log(`Your name is: ${name}`);
+    readline.close();
+    console.log("This important software is now closing");
+});
