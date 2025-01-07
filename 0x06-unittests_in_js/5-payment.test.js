40466a115ccf05f2d79c4af14e3bf35252376415
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-01-07 04:04:27 +0000

    check tests

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,32 +1,29 @@
-// Use spies and hooks ie.
-// beforeEach, afterEach to test the function sendPaymentRequestToApi
 const sinon = require('sinon');
 const { expect } = require('chai');
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./5-payment.js');
+const sendPaymentRequestToApi = require('./5-payment');
 
-describe('sendPaymentRequestToApi', function () {
-	let consoleSpy;
-	let functionStub;
+describe('sendPaymentRequestToApi', () => {
+  let bigBrother;
 
-	beforeEach(() => {
-		consoleSpy = sinon.spy(console, 'log');
-		functionStub = sinon.stub(Utils, 'calculateNumber');
-	});
+  beforeEach(() => {
+    if (!bigBrother) {
+      bigBrother = sinon.spy(console);
+    }
+  });
 
-	afterEach(() => {
-		consoleSpy.restore();
-		functionStub.restore();
-	});
+  afterEach(() => {
+    bigBrother.log.resetHistory();
+  });
 
-	it('should call Utils.calculateNumber', () => {
-		functionStub.returns(10);
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
+  });
 
-		sendPaymentRequestToApi(100, 20);
-
-		expect(functionStub.calledWithExactly('SUM', 100, 20)).to.be.true;
-		expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
-		expect(consoleSpy.calledOnce).to.be.true;
-		expect(Utils.calculateNumber(100, 20)).to.equal(10);
-	});
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
+  });
 });
