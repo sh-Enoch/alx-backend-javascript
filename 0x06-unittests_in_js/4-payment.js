aca678514f9d928c3eeb8261f00329d1fb7766ae
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-01-07 01:14:41 +0000

    payment

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,6 +1,8 @@
-const utils = require('./utils.js');
+const Utils = require('./utils.js');
+
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const payment = utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${payment}`);
+	const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${sum}`);
 }
+
 module.exports = sendPaymentRequestToApi;
