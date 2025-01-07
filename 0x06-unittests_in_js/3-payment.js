990f96b800b32dc0f970dcc40a4dd6cb93c93ffc
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-01-07 00:54:17 +0000

    payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,8 @@
-const utils = require('./utils.js');
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const payment = utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${payment}`);
-}
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
+
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,15 @@
-const chai = require("chai");
-const expect = chai.expect;
-// import sinon
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require('./3-payment.js');
-const utils = require('./utils.js');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-describe('sendPaymentRequestToApi', function() {
-  it('spies on the calculateNumber property', function() {
-    let mySpy = sinon.spy(utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    expect(mySpy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(mySpy.calledOnce).to.be.true;
-    mySpy.restore();
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
